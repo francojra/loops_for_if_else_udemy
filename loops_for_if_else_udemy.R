0fac7aea70d0019cda7bcbac479d932759772f37
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-11 21:26:37 -0300

    Add files via upload

--- loops_for_if_else_udemy.R
+++ loops_for_if_else_udemy.R
@@ -4,3 +4,29 @@
 # Data do script: 11/05/22 --------------------------------------------------------------------------------------------------
 # Referência: Curso Udemy ---------------------------------------------------------------------------------------------------
 
+# Loops ------------------------------------------------------------------------------------------------------------------------------------
+
+### Loops são traduzidos como laços/voltas/circuitos que são executados até uma função
+### cumprir um determinado comando.
+
+print("Feliz 2010")
+print("Feliz 2011")
+###...
+print("Feliz 2018")
+
+# Exemplos ---------------------------------------------------------------------------------------------------------------------------------
+
+for (ano in 2006:2018) {
+  print(paste("Feliz", ano))
+}
+
+### Para cada ano na sequência c(2006:2018), execute o código print(paste("Feliz", ano)).
+### Uma vez que o lopp tenha executado o código para cada ano do vetor, o loop termina.
+
+for (n in 1:10) {
+  print(n + 1)
+}
+
+### Esse método do loop também funciona para as funções da família apply.
+
+sapply(1:10, function(x) {x + 1})
